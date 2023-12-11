f0cf0dbf6ef203fefa1274463fbca85eb705405d
KR_Vroomen <145545563+KRVroomen@users.noreply.github.com>
2023-12-11 08:59:01 +0100

    Delete Queue__class.py
    .

--- Queue__class.py
+++
@@ -1,6 +0,0 @@
-from collections import deque
-
-class Queue:
-    def __init__(self,speed):
-        self.queue = deque()                    #The actual queue
-        self.waitingtime_patient = deque()     #Used to calculate the waiting time of a queue
